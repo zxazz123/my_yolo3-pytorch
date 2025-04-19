e63cc6544a4c7b51035eca2bb09e5795dfa8067c
chc <670742996@qq.com>
2025-04-19 21:55:51 +0800

    增加stop_at参数，用来设置运行完此epoch时退出。

--- yolo.py
+++ yolo.py
@@ -25,8 +25,8 @@ class YOLO(object):
         #   验证集损失较低不代表mAP较高，仅代表该权值在验证集上泛化性能较好。
         #   如果出现shape不匹配，同时要注意训练时的model_path和classes_path参数的修改
         #--------------------------------------------------------------------------#
-        "model_path"        : 'model_data/yolo_weights.pth',
-        "classes_path"      : 'model_data/coco_classes.txt',
+        "model_path"        : 'logs/best_epoch_weights.pth',
+        "classes_path"      : 'model_data/voc_classes.txt',
         #---------------------------------------------------------------------#
         #   anchors_path代表先验框对应的txt文件，一般不修改。
         #   anchors_mask用于帮助代码找到对应的先验框，一般不修改。
@@ -199,7 +199,11 @@ class YOLO(object):
 
             label = '{} {:.2f}'.format(predicted_class, score)
             draw = ImageDraw.Draw(image)
-            label_size = draw.textsize(label, font)
+            #label_size = draw.textsize(label, font)
+            bbox = draw.textbbox((0, 0), label, font=font)
+            width = bbox[2] - bbox[0]
+            height = bbox[3] - bbox[1]
+            label_size = (width, height)
             label = label.encode('utf-8')
             print(label, top, left, bottom, right)
             
